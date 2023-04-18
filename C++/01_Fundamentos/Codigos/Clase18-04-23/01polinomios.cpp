c9a032236ec5e5101e22afbe5c7fa54b5442a751
Aldo Santiago Alejandro <26aldo.santiago@gmail.com>
2023-04-18 00:32:01 -0600

    Se agregan codigos de la Clase 18-04-23

---
+++ C++/01_Fundamentos/Codigos/Clase18-04-23/01polinomios.cpp
@@ -0,0 +1,21 @@
+/**
+ - Archivo .h (headers)
+ - Sizeof() 
+ - Funciones
+ - Arreglos
+*/
+
+// Programa que realice suma
+
+#include <iostream>
+#include "polinomios.h" // header
+
+using namespace std;
+
+int main(){
+
+    int n; // Numero de terminos
+    n = pedirTam();
+    pedirValores(n);
+
+}
---
+++ C++/01_Fundamentos/Codigos/Clase18-04-23/prueba.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    int edad; // 4 bytes
+    char letra; // 1 byte
+    int arreglo[5]; // 20 bytes
+
+    cout<<sizeof(edad)<<endl;
+    cout<<sizeof(letra)<<endl;
+    cout<<sizeof(arreglo)<<endl;
+
+}
